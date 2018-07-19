be3fc2c45616b07dd7c89b27931984396262a277
Lucy Wall <lhwall@gmail.com>
2018-07-19 15:54:29 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,10 @@
 class SongsController < ApplicationController
   def index
+    if params[:artist_id]
+      @songs = Song.find(params[:artist_id]).posts
+    else 
     @songs = Song.all
+  end 
   end
 
   def show
